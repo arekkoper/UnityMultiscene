095645dc49ab84c2bd9ff905724116e374285ab0
Arkadiusz Koper <arkadiusz.koper1990@gmail.com>
2022-08-02 12:50:40 +0200

    Init commit

---
+++ Assets/_GAME/Scripts/Editor/SceneSwitcherWindow.cs
@@ -0,0 +1,51 @@
+#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace AQ.Tools
+{
+    public class SceneSwitchWindow : EditorWindow
+    {
+        private Vector2 scrollPos;
+
+        [MenuItem("Aquare/Scene Switch Window")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<SceneSwitchWindow>();
+            window.titleContent = new GUIContent("Scene Switcher");
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.BeginVertical();
+            scrollPos = EditorGUILayout.BeginScrollView(scrollPos, false, false);
+
+            GUILayout.Label("Scenes", EditorStyles.boldLabel);
+
+            for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+            {
+                var scene = EditorBuildSettings.scenes[i];
+                if (scene.enabled)
+                {
+                    var sceneName = Path.GetFileNameWithoutExtension(scene.path);
+                    var pressed = GUILayout.Button(i + ": " + sceneName, new GUIStyle(GUI.skin.GetStyle("Button")) { alignment = TextAnchor.MiddleLeft });
+
+                    if (pressed)
+                    {
+                        var activeScene = EditorSceneManager.GetActiveScene();
+                        EditorSceneManager.SaveScene(activeScene, activeScene.path);
+                        EditorSceneManager.OpenScene(scene.path);
+                    }
+                }
+            }
+
+            EditorGUILayout.EndScrollView();
+            EditorGUILayout.EndVertical();
+
+        }
+    }
+}
+#endif
